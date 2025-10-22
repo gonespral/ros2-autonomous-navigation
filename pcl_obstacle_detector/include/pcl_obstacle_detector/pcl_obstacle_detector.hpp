e744d9168f247abc746ef31a40bddb0b0dfb8b69
Gonçalo Fernández-Nespral Vaz <G.FernandezNespralVaz-1@student.tudelft.nl>
2025-10-22 15:46:25 +0200

    Set up basic publisher and subscriber node

--- pcl_obstacle_detector/include/pcl_obstacle_detector/pcl_obstacle_detector.hpp
+++ pcl_obstacle_detector/include/pcl_obstacle_detector/pcl_obstacle_detector.hpp
@@ -2,5 +2,7 @@
 #define SIMPLE_CONTROL_H
 
 #include <rclcpp/rclcpp.hpp>
-
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <vision_msgs/msg/detection3_d_array.hpp>
 #endif
+
--- pcl_obstacle_detector/src/pcl_obstacle_detector.cpp
+++ pcl_obstacle_detector/src/pcl_obstacle_detector.cpp
@@ -1,6 +1,75 @@
 #include "pcl_obstacle_detector/pcl_obstacle_detector.hpp"
+#include <string>
 
-int main() {
+class PCLNode: public rclcpp::Node {
+    public:
+        PCLNode(): Node("pcl_obstacle_detector_node")
+        {
+
+            // Get node args and set defaults
+            this->declare_parameter("point_cloud_topic", "/mirte/camera_depth/points");
+            this->declare_parameter("detections_topic", "/detections");
+            this->declare_parameter("publish_interval_ms", 100);
+            this->declare_parameter("queue_size", 1);
+
+            // Declare node configuration parameters
+            std::string point_cloud_topic = this->get_parameter("point_cloud_topic").as_string();  // sensor_msgs/msg/PointCloud2
+            std::string detections_topic = this->get_parameter("detections_topic").as_string();  // vision_msgs/msg/Detection3DArray
+            int publish_interval_ms = this->get_parameter("publish_interval_ms").as_int();
+            int queue_size = this->get_parameter("queue_size").as_int();
+
+            // Log info
+            RCLCPP_INFO(this->get_logger(), "Starting node '%s'...", this->get_name());
+
+            // Define callback functions
+            auto callback_point_cloud =
+                [this](sensor_msgs::msg::PointCloud2::UniquePtr msg) -> void {
+                    RCLCPP_INFO(
+                        this->get_logger(),
+                        "Received PointCloud2: frame_id='%s', width=%u, height=%u, fields=%zu",
+                        msg->header.frame_id.c_str(),
+                        msg->width,
+                        msg->height,
+                        msg->fields.size()
+                    );
+                };
+
+            // Create subscriber and publisher
+            subscription_point_cloud = 
+            this->create_subscription<sensor_msgs::msg::PointCloud2>(
+                point_cloud_topic, 
+                queue_size,
+                callback_point_cloud
+            );
+
+            publisher_detections = 
+            this->create_publisher<vision_msgs::msg::Detection3DArray>(
+                detections_topic,
+                queue_size
+            );
+
+            // Log info
+            RCLCPP_INFO(
+                this->get_logger(),
+                "Started node '%s'\nPoint cloud topic: '%s'\nDetections topic: '%s'\nPublish interval: %d ms\nQueue size: %d",
+                this->get_name(),
+                point_cloud_topic.c_str(),
+                detections_topic.c_str(),
+                publish_interval_ms,
+                queue_size
+            );
+
+        };
+
+    private:
+        rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr subscription_point_cloud;
+        rclcpp::Publisher<vision_msgs::msg::Detection3DArray>::SharedPtr publisher_detections;
+};
+
+int main(int argc, char *argv[]){
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<PCLNode>());
+    rclcpp::shutdown();
     return 0;
 }
 
